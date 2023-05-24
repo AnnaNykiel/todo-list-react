3d3149a6ad79687f709e122038f98db196043c84
Anna Nykiel <nykielanna94@gmail.com>
2023-05-24 13:40:51 +0200

    add empty task block

--- src/form/index.js
+++ src/form/index.js
@@ -6,7 +6,12 @@ const Form = ({ addNewTask }) => {
 
   const onForSubmit = (event) => {
     event.preventDefault();
-    addNewTask(newTaskContent.trim());
+const trimmedNewTaskContent= newTaskContent.trim();
+
+    if (!trimmedNewTaskContent) {
+      return;
+    };
+    addNewTask(trimmedNewTaskContent);
     setNewTaskContent("");
   };
   return (
